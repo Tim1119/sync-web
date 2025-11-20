4155711359dbf42be121a444a24a2fa5791f3438
Ajani Timothy <timothyinioluwa@gmail.com>
2025-11-20 07:08:54 +0100

    fix: worked on our uniqueness section

--- src/app/components/public/Uniqueness.tsx
+++ src/app/components/public/Uniqueness.tsx
@@ -65,10 +65,9 @@ const mobileFeatureVariant: Variants = {
 export default function UniquenessSection() {
   
   const features = [
-    { text: "No Annual Fees.", icon: "🗎", custom: 0 },
-    { text: "No Joining Fees.", icon: "💳", custom: 1 },
-    { text: "No Rewards Redemption Fees.", icon: "🎁", custom: 2 },
-    { text: "No Foreign Transaction Fees.", icon: "⚡", custom: 3 },
+    { text: "No Rewards Redemption Fees.", icon: "/landing/credit-card-cancelled.svg", custom: 2 },
+    { text: "No Annual Fees.", icon: "/landing/credit-card-cancelled.svg", custom: 0 },
+    { text: "No Joining Fees.", icon: "/landing/credit-card-cancelled.svg", custom: 1 },
   ];
 
   return (
@@ -79,7 +78,7 @@ export default function UniquenessSection() {
        <div className="absolute inset-0 z-0 w-full h-full">
             <Image
                 // src="/img.png"
-                src="/img.png"
+                src="/landing/uniqueness.png"
                 alt="Dark geometric background with blue lighting"
                 layout="fill"
                 objectFit="cover" 
@@ -118,15 +117,22 @@ export default function UniquenessSection() {
             {features.map((feature, index) => (
                 <motion.div
                     key={index}
-                    className="p-4 rounded-xl bg-[#030C32] border border-white/10 shadow-lg" 
+                    className="p-4 rounded-xl bg-[#030C32] border border-white/10 shadow-lg " 
                     initial="hidden"
                     whileInView="visible"
                     variants={mobileFeatureVariant}
                     custom={index}
                     viewport={{ once: true, amount: 0.2 }}
                 >
-                    <h3 className="text-base font-bold text-white flex items-center">
-                        <span className="inline-block mr-2 text-[#113CFC] text-lg">{feature.icon}</span> 
+                    <h3 className="text-base font-medium text-white flex items-center gap-2">
+                         <Image
+                        src="/landing/credit-card-cancelled.svg"
+                        className="object-contain"
+                        alt="Company Logo"
+                        width={30}
+                        height={30}
+                        />
+                        {/* <span className="inline-block mr-2 text-[#113CFC] text-lg">{feature.icon}</span>  */}
                         {feature.text}
                     </h3>
                 </motion.div>
@@ -187,46 +193,68 @@ export default function UniquenessSection() {
             
             {/* BOX 1: Top Left Corner */}
             <motion.div
-                className="absolute hidden md:block p-6 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40 "
-                style={{ top: '0', left: '0' }} 
+                className="absolute -left-[-310px] top-[40%] hidden md:block p-4 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40 "
+                // style={{ top: '0', left: '0' }} 
                 initial="hidden"
                 whileInView="visible"
                 variants={featureBoxVariant}
                 custom={0}
                 viewport={{ once: true, amount: 0.5 }}
             >
-                <h3 className="text-xl font-bold text-white mb-2 lg:bg-red-400">
-                    <span className="inline-block mr-2 text-[#113CFC]">🗎</span> No Annual Fees.
+                <h3 className="text-sm  font-medium text-white bg-[#030C32] w-[100px]">
+                    {/* <span className="inline-block mr-2 text-[#113CFC]">🗎</span> No Annual Fees. */}
+                     <Image
+                        src="/landing/credit-card-cancelled.svg"
+                        className="object-contain"
+                        alt="Company Logo"
+                        width={30}
+                        height={30}
+                        />
+                    
+                    No Annual Fees.
                 </h3>
             </motion.div>
 
             {/* BOX 2: Bottom Left Corner */}
             <motion.div
-                className="absolute hidden md:block p-6 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40"
-                style={{ bottom: '0', left: '0' }}
+                className="absolute -left-[-310px] top-[68%] hidden md:block p-4 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40 w-[130px]"
                 initial="hidden"
                 whileInView="visible"
                 variants={featureBoxVariant}
                 custom={1}
                 viewport={{ once: true, amount: 0.5 }}
             >
-                <h3 className="text-xl font-bold text-white mb-2">
-                    <span className="inline-block mr-2 text-[#113CFC]">💳</span> No Joining Fees.
+                <h3 className="text-sm font-medium text-white mb-2">
+                    <Image
+                        src="/landing/wallet-cancelled.svg"
+                        className="object-contain"
+                        alt="Company Logo"
+                        width={30}
+                        height={30}
+                        />
+                    No Joining Fees.
                 </h3>
             </motion.div>
 
             {/* BOX 3: Top Right Corner */}
             <motion.div
-                className="absolute hidden md:block p-6 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40"
-                style={{ top: '0', right: '0' }} 
+                className="absolute -left-[-770px] top-[55%] hidden md:block p-4 rounded-xl bg-[#030C32] border border-white/10 shadow-2xl z-40 w-[130px]"
+                // style={{ top: '0', right: '0' }} 
                 initial="hidden"
                 whileInView="visible"
                 variants={featureBoxVariant}
                 custom={2}
                 viewport={{ once: true, amount: 0.5 }}
             >
-                <h3 className="text-xl font-bold text-white mb-2">
-                    <span className="inline-block mr-2 text-[#113CFC]">🎁</span> No Rewards Redemption Fees.
+                <h3 className="text-sm font-medium text-white mb-2"> 
+                    <Image
+                        src="/landing/credit-card-cancelled.svg"
+                        className="object-contain"
+                        alt="Company Logo"
+                        width={30}
+                        height={30}
+                        />
+                        No Rewards Redemption Fees.
                 </h3>
             </motion.div>
         </div>
