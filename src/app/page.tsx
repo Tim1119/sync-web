467b14ad2ced6d58d01308f8d1da44446d6d9cf8
Ajani Timothy <timothyinioluwa@gmail.com>
2025-11-20 08:21:42 +0100

    fix cleaned up home page and removed unused section components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 
 import HeroClientsSection from "./components/public/HeroClientsSection";
-import ClientsSection from "./components/public/OurClients";
 import FeaturesSection from "./components/public/FeaturesSection";
-// import HeroClientsSection from "./components/public/hhherro";
-
-
-import LandingPage from "./components/public/LandingPage";
 import UniquenessSection from "./components/public/Uniqueness";
 import TargetAndFaqSection from "./components/public/TargetAndFaqSection";
 
@@ -14,26 +9,10 @@ import TargetAndFaqSection from "./components/public/TargetAndFaqSection";
 export default function Home() {
   return (
     <div className="w-full overflow-hidden">
-
-    
-       
       <HeroClientsSection />
-
       <FeaturesSection />
-
-
-      {/* <ClientsSection /> */}
-
-
       <UniquenessSection />
-
       <TargetAndFaqSection />
-
-      {/* <Footer /> */}
-
-      {/* <ClientsSection /> */}
-
-      {/* <LandingPage /> */}
     </div>
   );
 }
